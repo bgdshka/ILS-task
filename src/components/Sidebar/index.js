d37db6d5283b7ce4426c33dfedbb5593c419310a
Kirill Ateev <kirill.ateev59@gmail.com>
2022-03-18 15:31:21 +0300

    feat: add tags, select in adresses

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,18 @@
 import React, { useEffect } from "react";
-import { Table, Tag, Space } from "antd";
+import { Table, Tag, Space, Select } from "antd";
 import { Resizable } from "re-resizable";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as ordersActions from "../../actions/orders";
 import "antd/dist/antd.css";
+
+const { Option } = Select;
+
+const mapPoints = [
+  { id: 1, name: "Склад 1", coords: [55.7152942, 37.6390595] },
+  { id: 2, name: "Склад 2", coords: [55.792942, 37.6190595] },
+  { id: 3, name: "Склад 3", coords: [55.792942, 37.6890595] },
+];
 
 const columns = [
   {
@@ -17,23 +25,59 @@ const columns = [
     title: "Start",
     dataIndex: "start",
     key: "start",
-    render: coords => <div>{coords.join(", ")}</div>,
+    render: coords => {
+      const defaultCoordsString = coords.join(", ");
+      return (
+        <div>
+          <Select
+            defaultValue={defaultCoordsString}
+            onChange={value => console.log("value", value)}
+          >
+            <Option value={defaultCoordsString}>{defaultCoordsString}</Option>
+            {mapPoints.map(point => (
+              <Option key={point.id} value={point.coords.join(", ")}>
+                {point.name}
+              </Option>
+            ))}
+          </Select>
+        </div>
+      );
+    },
   },
   {
     title: "Finish",
     dataIndex: "finish",
     key: "finish",
-    render: coords => <div>{coords.join(", ")}</div>,
+    render: coords => {
+      const defaultCoordsString = coords.join(", ");
+      return (
+        <div>
+          <Select
+            defaultValue={defaultCoordsString}
+            onChange={value => console.log("value", value)}
+          >
+            <Option value={defaultCoordsString}>{defaultCoordsString}</Option>
+            {mapPoints.map(point => (
+              <Option key={point.id} value={point.coords.join(", ")}>
+                {point.name}
+              </Option>
+            ))}
+          </Select>
+        </div>
+      );
+    },
   },
   {
-    title: "Action",
-    key: "action",
-    render: (text, record) => (
-      <Space size="middle">
-        {console.log(text, record)}
-        <a>Edit</a>
-      </Space>
-    ),
+    title: "Tags",
+    key: "tags",
+    dataIndex: "tags",
+    render: (tags, record) => {
+      return tags.map(tag => (
+        <Tag color={tag === "Экспресс" ? "red" : "green"} key={tag}>
+          {tag.toUpperCase()}
+        </Tag>
+      ));
+    },
   },
 ];
 
